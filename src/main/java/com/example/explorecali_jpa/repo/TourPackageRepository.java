c950fd1fd47301b921f80a1bd3e45efb9a871be2
BhaveshSidhwani <sidhwanibhavesh@gmail.com>
2025-07-22 18:34:56 -0700

    Added OpenAPI dependency

--- src/main/java/com/example/explorecali_jpa/repo/TourPackageRepository.java
+++ src/main/java/com/example/explorecali_jpa/repo/TourPackageRepository.java
@@ -3,9 +3,11 @@ package com.example.explorecali_jpa.repo;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.example.explorecali_jpa.model.TourPackage;
 
+@RepositoryRestResource(path="packages", collectionResourceRel = "packages")
 public interface TourPackageRepository extends JpaRepository<TourPackage, String> {
     Optional<TourPackage> findByName(String name);
 }
