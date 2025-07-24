22c747b98458a543ebcb5f7c2166cb86ea39e039
BhaveshSidhwani <sidhwanibhavesh@gmail.com>
2025-07-23 22:37:39 -0700

    Added Global Exception handling

--- src/main/java/com/example/explorecali_jpa/web/TourRatingController.java
+++ src/main/java/com/example/explorecali_jpa/web/TourRatingController.java
@@ -2,12 +2,10 @@ package com.example.explorecali_jpa.web;
 
 import java.util.List;
 import java.util.Map;
-import java.util.NoSuchElementException;
 import java.util.Optional;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -83,11 +81,5 @@ public class TourRatingController {
         @PathVariable Integer customerId
     ) {
         tourRatingService.delete(tourId, customerId);
-    }
-
-    @ExceptionHandler(NoSuchElementException.class)
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    public String return404(NoSuchElementException exception) {
-        return exception.getMessage();
     }
 }
