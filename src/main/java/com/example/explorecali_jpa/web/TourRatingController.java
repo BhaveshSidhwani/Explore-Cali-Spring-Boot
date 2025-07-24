53509e136e9a1e97c100f8391c3a9eb4e6ed2739
BhaveshSidhwani <sidhwanibhavesh@gmail.com>
2025-07-23 22:11:17 -0700

    Added PUT, PATCH and DELETE endpoints for tour ratings

--- src/main/java/com/example/explorecali_jpa/web/TourRatingController.java
+++ src/main/java/com/example/explorecali_jpa/web/TourRatingController.java
@@ -3,8 +3,10 @@ package com.example.explorecali_jpa.web;
 import java.util.List;
 import java.util.Map;
 import java.util.NoSuchElementException;
+import java.util.Optional;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,6 +20,9 @@ import com.example.explorecali_jpa.model.TourRating;
 import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+
 
 @RestController
 @RequestMapping(path = "/tours/{tourId}/ratings")
@@ -38,8 +43,7 @@ public class TourRatingController {
     public Map<String, Double> getAverage(@PathVariable Integer tourId) {
         return Map.of("average", tourRatingService.getAverageRating(tourId));
     }
-    
-    
+
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public void createTourRating(
@@ -48,6 +52,37 @@ public class TourRatingController {
     ) {
         System.out.println("ratingDto: " + ratingDto);
         tourRatingService.createNew(tourId, ratingDto.getCustomerId(), ratingDto.getRating(), ratingDto.getComment());
+    }
+
+    @PutMapping
+    public RatingDto updateWithPut(
+        @PathVariable Integer tourId,
+        @RequestBody @Valid RatingDto rating
+    ) {
+        return new RatingDto(tourRatingService.update(tourId, rating.getCustomerId(), rating.getRating(), rating.getComment()));
+    }
+
+    @PatchMapping
+    public RatingDto updateWithPatch(
+        @PathVariable Integer tourId,
+        @RequestBody @Valid RatingDto rating
+    ) {
+        return new RatingDto(
+            tourRatingService.updateSome(
+                tourId,
+                rating.getCustomerId(),
+                Optional.ofNullable(rating.getRating()),
+                Optional.ofNullable(rating.getComment())
+            )
+        );
+    }
+
+    @DeleteMapping("{customerId}")
+    public void delete(
+        @PathVariable Integer tourId,
+        @PathVariable Integer customerId
+    ) {
+        tourRatingService.delete(tourId, customerId);
     }
 
     @ExceptionHandler(NoSuchElementException.class)
